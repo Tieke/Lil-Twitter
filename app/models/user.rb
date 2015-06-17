a849b8213932738afa7335911c58526fb56b4cab
Te Aihe Butler <apprentice@StMac2.local>
2015-06-18 11:42:48 +1200

    Login/Logout Done. Not tested yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+
+   def self.authenticate_email(email, password)
+    @user = User.find_by(email: email)
+    if @user
+      User.authenticate_password(password)
+    end
+  end
+
+  def self.authenticate_password(password)
+    if @user.password == password
+      @user
+    end
+  end
+
 end
